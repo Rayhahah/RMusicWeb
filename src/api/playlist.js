c7e1d92a5b9aba4e62af7afd57fa2e9239fa2f53
Rayhahah <leizhkkk@126.com>
2021-11-29 00:50:21 +0800

    base Url

--- src/api/playlist.js
+++ src/api/playlist.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 
 // const baseURL = "http://localhost:3000"
-const baseURL = "http://47.252.86.216:3000"
+const baseURL = "http://rmusic.ccoolworld.com:3000"
 export function fetchList(params) {
     return request({
         params,
